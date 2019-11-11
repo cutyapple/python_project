2df0ff0dee43a29b45b2bd1d9846b5398dc59d9e
cutyapple <dbtldhs0915@naver.com>
2019-11-11 23:36:58 +0900

    Chess Start!

---
+++ Chess.py
@@ -0,0 +1,44 @@
+# 2019-11-11 Chess with Python By CutyApple
+# The white(bottom) is upper and the black(top) is lower
+# King, Queen, Bishop, Knight, Rook, Pawn
+
+chessTable = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+
+for i in range(0, 8):
+    chessTable[0][0] = '♜'
+    chessTable[0][1] = '♞'
+    chessTable[0][2] = '♝'
+    chessTable[0][3] = '♛'
+    chessTable[0][4] = '♚'
+    chessTable[0][5] = '♝'
+    chessTable[0][6] = '♞'
+    chessTable[0][7] = '♜'
+    chessTable[1][i] = '♟'
+
+    chessTable[7][0] = '♖'
+    chessTable[7][1] = '♘'
+    chessTable[7][2] = '♗'
+    chessTable[7][3] = '♔'
+    chessTable[7][4] = '♕'
+    chessTable[7][5] = '♗'
+    chessTable[7][6] = '♘'
+    chessTable[7][7] = '♖'
+    chessTable[6][i] = '♙'
+
+def printTable():
+    for i in range(0, 8):
+        for j in range(0, 8):
+            print(chessTable[i][j], end=' ')
+        print('')
+
+printTable()
